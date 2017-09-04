c47e225a27ef5f5e5ccd585eb34efec421588013
pjakapan <j.petchan@togetherteam.co.th>
2017-09-04 15:58:08 +0700

    change directory

--- tests/UI/google.js
+++ tests/UI/google.js
@@ -1,10 +1,17 @@
-var host = 'http://www.google.com';
+var host = 'https://www.google.co.th';
 module.exports = {
   'some test': function (client) {
     client
       .url(host)
-      .waitForElementVisible('body', 1000)   
+      .waitForElementVisible('body', 1000)
       .assert.urlContains('google')
+      .pause(2000)
+  },
+  'some test 2': function (client) {
+    client
+      .assert.urlContains('google')
+      .pause(1000)
+      .assert.elementPresent('#tsf > div.tsf-p > div.jsb > center > input[type="submit"]:nth-child(1)')
       .pause(2000)
       .end();
   }
